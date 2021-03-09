29831d71a7919ed74d5abe304bed93cda365410e
Hannes F <62026775+afy@users.noreply.github.com>
2021-03-09 22:58:32 +0100

    Added comments / cleanup

--- src/lab5/market/MarketEvent.java
+++ src/lab5/market/MarketEvent.java
@@ -3,12 +3,29 @@ import lab5.State;
 import lab5.event.Event;
 import lab5.event.EventQueue;
 
+/**
+ * Abstract class, extension from Event. Only meant as a "connection" for marketEvents.
+ * @author Simon Engström, Hannes Furhoff, Emil Wiklund, Johannes Sundström
+ */
 public abstract class MarketEvent extends Event{
+	
+	/**
+	 * Customer the event regards. Events that need customer data will override constructor to include it
+	 */
 	public Customer customer;
+	
+	/**
+	 * Constructor
+	 * @param time Current simulation time
+	 */
 	public MarketEvent(double time) {
 		super(time);
 	}
 
-	abstract public void runEvent(State state, EventQueue queue);
-	
+	/**
+	 * 
+	 * @param state
+	 * @param queue
+	 */
+	abstract public void runEvent(State state, EventQueue queue);	
 }
