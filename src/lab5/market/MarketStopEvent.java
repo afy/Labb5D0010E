ce77c4018e8e0aad4be8c50a9911c25e78393b6c
Hannes F <62026775+afy@users.noreply.github.com>
2021-03-09 22:53:54 +0100

    Added comments / cleanup

--- src/lab5/market/MarketStopEvent.java
+++ src/lab5/market/MarketStopEvent.java
@@ -4,12 +4,25 @@ import lab5.State;
 import lab5.event.EventQueue;
 import lab5.event.StopEvent;
 
+/**
+ * Extension of abstract class StopEvent, run at stop of simulation
+ * @author Simon Engström, Hannes Furhoff, Emil Wiklund, Johannes Sundström
+ */
 public class MarketStopEvent extends StopEvent{
 	
+	/**
+	 * Constructor
+	 * @param time Current (simulated) time
+	 */
 	public MarketStopEvent(double time) {
 		super(time);
 	}
 
+	/**
+	 * Stop simulation and notify state
+	 * @param state
+	 * @param eventQueue
+	 */
 	protected void postRunEvent(State state, EventQueue eventQueue) {
 		state.stopSim();
 		((MarketState)state).recivedChange();
