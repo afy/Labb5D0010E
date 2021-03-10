bdb7f33fa312c6e400114462eedeb9d081a530f4
Sinoeng <80058456+Sinoeng@users.noreply.github.com>
2021-03-10 10:06:18 +0100

    Add files via upload

--- src/lab5/State.java
+++ src/lab5/State.java
@@ -3,22 +3,27 @@ package lab5;
 import java.util.*;
 
 /**
- * 
- * @author Wiklund
+ * An object of State represents the state of something that is supposed to be simulated, e.g. a supermarket, a hockey match or something else.
+ * The specific state needs to contain its own set of relevant variables.
+ *
+ * @author Simon Engström, Hannes Furhoff, Emil Wiklund, Johannes Sundström
  *
  **/
-
 public class State extends Observable{
+
 	
-	/** 
-	* 
-	**/
-	
-	protected boolean _stopSim = false;
-	
+	protected boolean _stopSim = false; //
+
+	/**
+	 * Stops the simulation.
+	 */
 	public void stopSim() {
 		_stopSim = true;
 	}
+
+	/**
+	 * Makes sure that the simulation can run before it is started.
+	 */
 	public void startSim() {
 		_stopSim = false;
 	}
