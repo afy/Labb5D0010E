116f152b220855fafcc69af3bdd40aa497f22620
Sinoeng <80058456+Sinoeng@users.noreply.github.com>
2021-03-10 10:06:31 +0100

    Add files via upload

--- src/lab5/View.java
+++ src/lab5/View.java
@@ -2,8 +2,14 @@ package lab5;
 
 import java.util.*;
 
+/**
+ * View is supposed to give a visual representation of what is happening in the state during a simulation.
+ *
+ * @author Simon Engström, Hannes Furhoff, Emil Wiklund, Johannes Sundström
+ */
 abstract public class View implements Observer{
-	
+
+
 	abstract public void update(Observable o, Object arg);
 	
 }
